02b6ee578c17b6ebb44f1f810cdfe3b189b0fd36
durancer <1160888352@qq.com>
2023-01-13 23:28:40 +0800

    移出登录扫码状态位置

---
+++ src/main/java/com/xueyu/constant/ScanStatus.java
@@ -0,0 +1,38 @@
+package com.xueyu.constant;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * 登录扫码状态
+ *
+ * @author durance
+ */
+@AllArgsConstructor
+public enum ScanStatus {
+
+	/**
+	 * 等待扫码
+	 */
+	WAIT("wait"),
+
+	/**
+	 * 已扫码
+	 */
+	ING("ing"),
+
+	/**
+	 * 认证成功
+	 */
+	SUCCESS("success"),
+
+	/**
+	 * 取消认证
+	 */
+	CANCEL("cancel");
+
+	/**
+	 * 扫码状态
+	 */
+	private String status;
+
+}
