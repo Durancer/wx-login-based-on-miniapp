d0c64263028fe61f173151c6a3f4508f0d70b728
durancer <1160888352@qq.com>
2023-02-10 19:34:01 +0800

    将小程序信息转移到配置文件里进行读取

--- src/main/java/com/xueyu/constant/WxLoginContant.java
+++ src/main/java/com/xueyu/constant/WxLoginContant.java
@@ -15,24 +15,4 @@ public class WxLoginContant {
 	 */
 	public final static String SECRET = "";
 
-	/**
-	 * 小程序码状态 等待扫码
-	 */
-	public static final String STATUS_WAIT = "wait";
-
-	/**
-	 * 小程序码状态 用户已扫码
-	 */
-	public static final String STATUS_ING = "ing";
-
-	/**
-	 * 小程序码状态 用户已认证
-	 */
-	public static final String STATUS_SUCCESS = "success";
-
-	/**
-	 * 小程序码状态 用户已取消
-	 */
-	public static final String STATUS_CANCEL = "cancel";
-
 }
